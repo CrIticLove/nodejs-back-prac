172a14632dba1142aa4498c713d6baa093283274
Jack <786322423@qq.com>
2020-12-24 18:09:20 +0800

    cors setting modified

--- app.js
+++ app.js
@@ -10,14 +10,14 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // cross origin
-//allow custom header and CORS
+// allow custom header and CORS
 app.all('*', function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
   res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
 
   if (req.method == 'OPTIONS') {
-    res.send(200); /让options请求快速返回/
+    res.send(200); // 让options请求快速返回
   }
   else {
     next();
